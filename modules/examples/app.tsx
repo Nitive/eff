83fa3ed3d1708515f579cd9f980a493d468b54c7
Maxim Samoilov <nitive@icloud.com>
2018-10-11 23:33:05 +0100

    Better example

--- modules/examples/app.tsx
+++ modules/examples/app.tsx
@@ -1,9 +1,19 @@
 import { run } from '@eff/core/run'
 import { makeDomDriver } from '@eff/dom/client'
 import * as Snabbdom from '@eff/dom/h'
+import xs from 'xstream'
 
 function App() {
-  return [<div>div</div>, <button>button</button>]
+  const counter = xs
+    .periodic(1000)
+    .take(100)
+    .map(x => <div>Count: {x + 1}</div>)
+
+  return [
+    <div>div</div>,
+    <button>button</button>,
+    counter,
+  ]
 }
 
 run(<div><App /></div>, {
