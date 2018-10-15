689dacde2f0d09f5dd9bb05ec5ccfebe1fec699d
Maxim Samoilov <nitive@icloud.com>
2018-10-16 00:16:00 +0100

    Added test for refs on element with plain text inside
    because snabbdom handles such elements differently

--- modules/@eff/tests/dom.test.tsx
+++ modules/@eff/tests/dom.test.tsx
@@ -22,4 +22,28 @@ describe('dom', () => {
     document.querySelector('button')!.click()
     expect(document.body.innerHTML).toBe('<button>1</button>')
   })
+
+  it('should support refs on element with plain text inside', async () => {
+    function App(_props: any, sources: { DOM: DOMSource }) {
+      const buttonRef = sources.DOM.createRef()
+      const clicksCount$  = buttonRef.events('click').fold(x => x + 1, 0)
+
+      return (
+        <div>
+          <button ref={buttonRef}>text</button>
+          {clicksCount$}
+        </div>
+      )
+    }
+
+    document.body.innerHTML = '<div id="app">Loading...</div>'
+
+    run(<App />, {
+      DOM: makeDomDriver('#app'),
+    })
+
+    expect(document.body.innerHTML).toBe('<div><button>text</button>0</div>')
+    document.querySelector('button')!.click()
+    expect(document.body.innerHTML).toBe('<div><button>text</button>1</div>')
+  })
 })
